a556da38c655873f93d54bf7e24e8dccd959b138
chen.xin <cxthinking@163.com>
2018-12-23 09:22:16 +0000

    add app code

---
+++ phalcon-micro/app/app.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Local variables
+ * @var \Phalcon\Mvc\Micro $app
+ */
+
+/**
+ * Add your routes here
+ */
+$app->get('/', function () use($app) {
+    $sql = 'SELECT * FROM users';
+    $users = $app->db->fetchAll($sql);
+    var_export($users);
+});
+
+/**
+ * Not found handler
+ */
+$app->notFound(function () use($app) {
+    $app->response->setStatusCode(404, "Not Found")->sendHeaders();
+    echo $app['view']->render('404');
+});
